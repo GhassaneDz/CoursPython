38ee664c4eedb442eced1be2d3792d1c77f35dfa
GhassaneDz <med.kheznadji@gmail.com>
2019-11-05 13:52:13 +0100

    adding functions input and print ...

--- san-antonio.py
+++ san-antonio.py
@@ -13,11 +13,26 @@ characters = [
     "le chat potté",
     "kirikou"
     ]
+
+def get_random_quote():
+    # ge a random number
+    # get a quote from an array
+    # show the quote in the intepreter. print()
+    pass
+get_random_quote()
+
+def get_random_item_in(my_list):
+    #TODO : get a random item
+    item = my_list[0] #get in item from a list. For the moment, just get the first one.
+    print(item)
+    return "program is over" #returned value
+print(get_random_item_in(quotes))
+
+user_answer = input('Tapez entrée pour découvrir une autre citation ou B pour quitter le programme.')    
 # Show random quote
-if user_answer == "B":
-  pass
-elif user_answer == "C":
-  print("C pas la bonne réponse! Et pas d'humour, je c..")
-else :
-# - show another quote
-    
+#if user_answer == "B":
+# pass
+#elif user_answer == "C":
+  #print("C pas la bonne réponse! Et pas d'humour, je c..")
+#else :
+# show another quote
